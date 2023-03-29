99146b79c3abe9257f18bc828b04e34192b49c09
Rodrigo <89955839+RodrigoDjc@users.noreply.github.com>
2023-03-28 21:05:25 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,6 @@ const App = () => {
   return (
     <div className="h-full flex flex-col justify-center items-center p-10">
       <Card user={user} />
-      <Card user={user} />
       <Button cambiar={cambiar} />
     </div>
   );
